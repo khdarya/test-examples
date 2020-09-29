26d0008a052411156ac3262c8b4bb62583c1044f
khdarya <darya.khakhlova@gmail.com>
2020-09-29 10:11:04 -0400

    18 useCallback

--- src/stories/UseMemoUseCallback.stories.tsx
+++ src/stories/UseMemoUseCallback.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 
 export default {
     title: 'useMemo'
@@ -79,3 +79,50 @@ export const HelpsToReactMemo = () => {
         <Users users={newArray}/>
     </>
 }
+
+
+//callBack
+
+export const LikeUseCallback = () => {
+    console.log("LikeUseCallback")
+    const [counter, setCounter] = useState(0);
+    const [books, setBooks] = useState(["React", "JS", "CSS", "HTML"]);
+
+
+    const memoizedAddBook = useMemo(() => {
+        return  () => {
+            console.log(books)
+            const newUsers = [...books, 'Angular' + new Date().getTime()];
+            setBooks(newUsers);
+        }
+    }, [books]);
+
+
+    const memoizedAddBook2 = useCallback(() => {
+            console.log(books)
+            const newUsers = [...books, 'Angular' + new Date().getTime()];
+            setBooks(newUsers);
+    }, [books]);
+
+
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+        <Book addBook={memoizedAddBook}/>
+    </>
+}
+
+type BookSecretPropsType = {
+    addBook: ()=> void
+}
+
+const BooksSecret = (props: BookSecretPropsType) => {
+
+    console.log('BooksSecret')
+    return <div>
+        <button onClick={() => props.addBook()}>add book</button>
+    </div>
+}
+
+const Book = React.memo(BooksSecret);
